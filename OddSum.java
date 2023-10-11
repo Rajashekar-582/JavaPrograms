67be6a8f6be18486a45c9f0974b921813af10ed6
Rajashekar nerella <104785884+Rajashekar-582@users.noreply.github.com>
2023-10-11 16:55:43 +0530

    Add files via upload

---
+++ OddSum.java
@@ -0,0 +1,20 @@
+package corejava;
+
+public class OddSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=7,sum=0;
+		while(n<=21)
+		{
+			sum=sum+n;
+			n+=2;
+			
+		}
+		System.out.println(sum);
+			
+
+	}
+
+}
+
